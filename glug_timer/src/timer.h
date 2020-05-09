c06ae8d18afd39957c12019f35e3744748cc7e7c
Dustin Cook <dustin@libglug.org>
2020-05-08 23:33:11 -0700

    introduce allocators, remove internal stddef (#5)

--- glug_timer/src/timer.h
+++ glug_timer/src/timer.h
@@ -1,6 +1,7 @@
 #ifndef GLUG_STRUCT_TIMER_H
 #define GLUG_STRUCT_TIMER_H
 
+#include <glug/allocator_t.h>
 #include <glug/timer/time_t.h>
 #include <glug/timer/timer_state.h>
 
@@ -8,6 +9,7 @@
 
 struct glug_timer
 {
+    glug_free_t free;
     uint64_t start_clock, pause_clock, split_clock;
     uint64_t pause_split, pause_total;
     enum glug_timer_state state;
