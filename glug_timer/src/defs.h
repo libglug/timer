3ba514a7430678a319363ed41d18286c84841786
Dustin Cook <dustin@libglug.org>
2020-12-23 20:07:20 -0800

    rename defs to be more descriptive (#25)

--- glug_timer/src/defs.h
+++ glug_timer/src/defs.h
@@ -3,7 +3,7 @@
 
 #define NSEC_PER_SEC (uint32_t)1e9
 
-#define HIGH_U64(x) ((x) >> 32 & 0xffffffff)
-#define LOW_U64(x)  ((x) & 0xffffffff)
+#define HIGH_MASK_U64(x) ((x) & 0xffffffff00000000)
+#define LOW_MASK_U64(x)  ((x) & 0x00000000ffffffff)
 
 #endif // GLUG_DEFS_H
--- glug_timer/src/tick.c
+++ glug_timer/src/tick.c
@@ -12,10 +12,10 @@ void scale_to_time(const uint64_t *ticks, const struct frac *scale, struct glug_
     // a potential loss of precision
     double lfscale = (double)scale->numer / scale->denom;
 
-    uint64_t high = (HIGH_U64(*ticks) << 32) * lfscale;
+    uint64_t high = (uint64_t)(HIGH_MASK_U64(*ticks) * lfscale);
     lldiv_t highdiv = lldiv(high, NSEC_PER_SEC);
 
-    uint64_t low = (uint64_t)(LOW_U64(*ticks) * lfscale + 0.5) + highdiv.rem;
+    uint64_t low = (uint64_t)(LOW_MASK_U64(*ticks) * lfscale + 0.5) + highdiv.rem;
     lldiv_t lowdiv = lldiv(low, NSEC_PER_SEC);
 
     time->nsec = lowdiv.rem;
